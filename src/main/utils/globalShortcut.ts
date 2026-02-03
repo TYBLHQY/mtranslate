c19582074f31e707746e542a3f26d295795da354
MYQ <jerry200223200223@126.com>
2026-02-04 06:10:41 +0800

    fix: copyText delay

--- src/main/utils/globalShortcut.ts
+++ src/main/utils/globalShortcut.ts
@@ -5,13 +5,15 @@ import { showMainWindow, toggleMainWindow } from "./window";
 
 export function registerGlobalShortcut(mainWindow: BrowserWindow, shortcuts: Shortcut): void {
   if (!shortcuts.key) return;
+
   if (shortcuts.id === "openAndClose")
     globalShortcut.register(shortcuts.key, () => {
       toggleMainWindow(mainWindow);
     });
-  if (shortcuts.id === "copyText")
+  else if (shortcuts.id === "copyText")
     globalShortcut.register(shortcuts.key, async () => {
       const selectedText = getSelectedText();
+      await new Promise(_ => setTimeout(_, 100));
       mainWindow.webContents.send("selected-text", selectedText);
       showMainWindow(mainWindow);
     });
